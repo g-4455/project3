affe2869dd8083cabf8aab4a49e2e8f7f59a0306
Gram <grantms4455@gmail.com>
2024-12-07 21:38:56 -0700

    Fixed Abstraction

--- 3/src/Creature.java
+++ 3/src/Creature.java
@@ -1,4 +1,4 @@
-public interface Creature {
+public abstract class Creature {
     // Source code is decompiled from a .class file using FernFlower decompiler.
    String name = "";
    String type = "";
@@ -6,10 +6,11 @@ public interface Creature {
    int price = 0;
    int num = 0;
 
-   private static void animal(){};
-   private static void die(){};
-   private static void reproduce(){};
-   private static void getprint(){};
+   //public abstract Animal Animal(String n, String t, int g);
+   public abstract void die(int index);
+   public abstract Animal randomize();
+   public abstract Animal reproduce(Animal d, Animal e);
+   //public abstract String getprint(Animal g);
 
    //print not listed as it is a debug function
     
